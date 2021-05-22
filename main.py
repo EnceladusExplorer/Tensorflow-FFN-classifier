9afe5cc0c71b928a303da12a8ee8dc7c8179e0c6
Dino-Might <ranason90@ghotmail.com>
2021-05-23 00:55:47 +0100

    Added callbacks

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import tensorflow as tf
 from tensorflow.keras import models
 from tensorflow.keras.layers import Flatten, Dense, Conv2D, Dropout
+from tensorflow.keras.callbacks import EarlyStopping, ReduceLROnPlateau
 
 mnist = tf.keras.datasets.mnist
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-model_type = 'FNN'
+model_type = 'CNN'
 
 model = None
 
-if model_type is 'CNN':
+if model_type == 'CNN':
 	X_train = X_train.reshape(60000,28,28,1)
 	X_test = X_test.reshape(10000,28,28,1)
 
@@ -18,6 +19,7 @@ if model_type is 'CNN':
 
 	model = models.Sequential([
 		Conv2D(32, kernel_size=3, activation='relu', input_shape=(28, 28, 1)),
+		Conv2D(16, kernel_size=3, activation='relu'),
 		Conv2D(16, kernel_size=3, activation='relu'),
 		Flatten(),
 		Dense(10, activation='softmax')
@@ -48,12 +50,20 @@ else:
 	)
 
 
+callbacks = [
+    EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=0, mode='min'),
+    #TensorBoard(log_dir='./board_logs'),
+    ReduceLROnPlateau(monitor='val_loss', factor=0.5, patience=5, verbose=0, mode='min', min_delta=0.0001, cooldown=0, min_lr=0)
+]
+
 model.fit(
 		X_train,
 		y_train,
-		epochs=5,
-		batch_size=64,
-		steps_per_epoch=3
+		validation_data=(X_test, y_test),
+		epochs=5000,
+		batch_size=32,
+		steps_per_epoch=1024,
+		callbacks=callbacks
 	)
 
 model.evaluate(X_test, y_test)
